93d724913b337df3d7c3a36cc2aa3f10faac00c9
afgantaufiq <ganafgan@gmail.com>
2020-12-24 23:02:58 +0700

    update midtrans

--- foodmarket-backend/app/Http/Controllers/API/UserController.php
+++ foodmarket-backend/app/Http/Controllers/API/UserController.php
@@ -10,6 +10,7 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -98,5 +99,46 @@ class UserController extends Controller
         $token = $request->user()->currentAccessToken()->delete();
 
         return ResponseFormatter::success($token, 'Token Revoked');
+    }
+
+    public function fetch(Request $request)
+    {
+        return ResponseFormatter::success(
+            $request->user(), 'Data profile user berhasil di ambil'
+        );
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $data = $request->all();
+
+        $user = Auth::user();
+        $user->update($data);
+
+        return ResponseFormatter::success($user, 'Profile Updated');
+    }
+
+    public function updatePhoto(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'file' => 'required|image|max:2048'
+        ]);
+
+        if ($validator->fails()) {
+            return ResponseFormatter::error([
+                'error' => $validator->errors()
+            ], 'Update pho fails', 401);
+        }
+
+        if ($request->file('file')) {
+            $file = $request->file->store('assets/user', 'public');
+        }
+
+        //simpan foto ke database
+        $user = Auth::user();
+        $user->profile_photo_path = $file;
+        $user->update();
+
+        return ResponseFormatter::success([$file], 'Successfully Upload');
     }
 }
